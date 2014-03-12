720cf93ee71ec3c7c38fcb72301f5a0da0d80bdc
dylano <d@doliver.net>
2014-03-12 14:22:48 -0700

    Update owners.js
    Add component owners for Faramarz's team.

--- owners.js
+++ owners.js
@@ -7,7 +7,14 @@ var OWNERS = {
   "Build Config": "Dylan Oliver",
   "Gaia": "Vivien Nicolas",
   "Gaia::Contacts": "David Scravaglieri",
-  "Gaia::Email": "Andrew Sutherland",
-  "Gaia::Calendar": "James Lal",
-  "Gaia::Camera": "David Flanagan"
+  "Gaia::Calendar": "Dylan Oliver",
+  "Gaia::Clock": "Dylan Oliver",
+  "Gaia::E-Mail": "Dylan Oliver",
+  "Gaia::Gallery": "Hema Koka",
+  "Gaia::Camera": "Hema Koka",
+  "Gaia::Music": "Hema Koka",
+  "Gaia::Ringtones": "Hema Koka",
+  "Gaia::Dialer": "David Scravaglieri",
+  "Gaia::SMS": "David Scravaglieri",
+  "Gaia::Cost Control": "David Scravaglieri",
 };

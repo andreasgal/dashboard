4b85ece50ed6856b134a5b3fbed697d0541b5366
Gregor Wagner <anygregor@gmail.com>
2014-03-12 15:55:59 -0700

    Update owners.js

--- owners.js
+++ owners.js
@@ -2,12 +2,31 @@
 /* vim: set shiftwidth=2 tabstop=2 autoindent cindent expandtab: */
 
 var OWNERS = {
+  "AudioChannel": "Marco Chen",
+  "Bluetooth": "Marco Chen",
+  "Build Config": "Dylan Oliver",
   "DOM: Core & HTML": "Andrew Overholt",
   "DOM": "Andrew Overholt",
-  "Build Config": "Dylan Oliver",
   "Gaia": "Vivien Nicolas",
+  "Gaia::Browser": "Gregor Wagner".
+  "Gaia::Calendar": "Dylan Oliver",
+  "Gaia::Camera": "David Flanagan",
+  "Gaia::Clock": "Dylan Oliver",
   "Gaia::Contacts": "David Scravaglieri",
-  "Gaia::Email": "Andrew Sutherland",
-  "Gaia::Calendar": "James Lal",
-  "Gaia::Camera": "David Flanagan"
+  "Gaia::Dialer": "David Scravaglieri",
+  "Gaia::E-Mail": "Andrew Sutherland",
+  "Gaia::Everything.me": "Gregor Wagner",
+  "Gaia::First Time Experience": "Gregor Wagner",
+  "Gaia::FMRadio": "Hema Koka",
+  "Gaia::Gallery": "Hema Koka",
+  "Gaia::Homescreen": "Gregor Wagner",
+  "Gaia::Keyboard": "Tim Chien",
+  "Gaia::Search": "Gregor Wagner",
+  "Gaia::Settings": "Tim Chien",
+  "Gaia::SMS": "David Scravaglieri",
+  "Gaia::System": "Tim Chien & Gregor Wagner",
+  "Gaia::System::Browser Chrome": "Gregor Wagner",
+  "Geolocation": "Doug Turner",
+  "Performance": "Mike Lee",
+  "Graphics": "Milan Sreckovic",
 };

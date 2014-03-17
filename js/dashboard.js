591ff4cdb380dbb4fdeb16f3b9c8469ede6dae3a
Dylan Oliver <doliver@mozilla.com>
2014-03-17 12:59:49 -0700

    Move owner styling into dashboard.css

--- js/dashboard.js
+++ js/dashboard.js
@@ -181,7 +181,7 @@ function refresh() {
     var showOwners = $("div#toggleOwners").hasClass("checked");
     eachAlphabetically(components, function (component, counts) {
       var label = (showOwners && (component in OWNERS))
-                  ? component + "<p style='font-style: italic;font-size: 75%'>" + OWNERS[component] + "</p>"
+                  ? component + "<p class='componentowner'>" + OWNERS[component] + "</p>"
                   : component;
       html += "<li>";
       html += "<div class='component'>" + label + "</div>";

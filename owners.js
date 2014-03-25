5f7f8a3c0bd1aabef91cecc3ffc7ded17c9d9e8c
Gregor Wagner <anygregor@gmail.com>
2014-03-24 17:28:04 -0700

    Update owners.js

--- owners.js
+++ owners.js
@@ -14,47 +14,47 @@ var OWNERS = {
   // "Emulator": "?", // Firefox OS
   // "FxA": "?", // Firefox OS
   "DOM: Apps": "Fabrice Desré",
-  "DOM: Core & HTML": "Andrew Overholt (2nd: Johnny Stenback)", // DOM
+  "DOM: Core & HTML": "Andrew Overholt (Johnny Stenback)", // DOM
   "DOM": "Andrew Overholt (2nd: Johnny Stenback)", // DOM
-  "DOM: Contacts": "David Scravaglieri", // DOM
-  "DOM: Device Interfaces": "Andrew Overholt (2nd: Johnny Stenback)", // DOM
-  "DOM: Events": "Andrew Overholt (2nd: Johnny Stenback)",
-  "Gaia": "Dylan Oliver (2nd: Gregor Wagner)",
+  "DOM: Contacts": "David Scravaglieri (Anthony Ricaud)", // DOM
+  "DOM: Device Interfaces": "Andrew Overholt (Johnny Stenback)", // DOM
+  "DOM: Events": "Andrew Overholt (Johnny Stenback)",
+  "Gaia": "Dylan Oliver (Gregor Wagner)",
   "Gaia::Bluetooth File Transfer": "Tim Guan-tin Chien",
-  "Gaia::Browser": "Gregor Wagner (2nd: Ben Francis)",
+  "Gaia::Browser": "Gregor Wagner (Ben Francis)",
   "Gaia::Build": "Tim Guan-tin Chien",
   "Gaia::Calendar": "Dylan Oliver",
-  "Gaia::Camera": "Hema Koka",
+  "Gaia::Camera": "Hema Koka (David Flanagan)",
   "Gaia::Clock": "Dylan Oliver",
-  "Gaia::Contacts": "David Scravaglieri",
-  "Gaia::Cost Control": "David Scravaglieri",
-  "Gaia::Dialer": "David Scravaglieri",
+  "Gaia::Contacts": "David Scravaglieri (Anthony Ricaud)",
+  "Gaia::Cost Control": "David Scravaglieri (Anthony Ricaud)",
+  "Gaia::Dialer": "David Scravaglieri (Anthony Ricaud)",
   "Gaia::E-Mail": "Dylan Oliver",
-  "Gaia::Everything.me": "Gregor Wagner (2nd: Michael Henretty)",
-  "Gaia::First Time Experience": "Gregor Wagner (2nd: Michael Henretty)",
-  "Gaia::FMRadio": "Hema Koka",
-  "Gaia::Gallery": "Hema Koka",
+  "Gaia::Everything.me": "Gregor Wagner (Michael Henretty)",
+  "Gaia::First Time Experience": "Gregor Wagner (Michael Henretty)",
+  "Gaia::FMRadio": "Hema Koka (David Flanagan)",
+  "Gaia::Gallery": "Hema Koka (David Flanagan)",
   // "Gaia::GithubBot": "?",ven
-  "Gaia::Homescreen": "Gregor Wagner (2nd: Kevin Grandon)",
+  "Gaia::Homescreen": "Gregor Wagner (Kevin Grandon)",
   "Gaia::Keyboard": "Tim Guan-tin Chien",
-  "Gaia::Music": "Hema Koka",
+  "Gaia::Music": "Hema Koka (David Flanagan)",
   "Gaia::Notes": "Dylan Oliver",
   // "Gaia::PDF Viewer": "?",
   // "Gaia::PerformanceTest": "?",
-  "Gaia::Ringtones": "Hema Koka",
-  "Gaia::Search": "Gregor Wagner (2nd: Kevin Grandon)",
+  "Gaia::Ringtones": "Hema Koka (David Flanagan)",
+  "Gaia::Search": "Gregor Wagner (Kevin Grandon)",
   "Gaia::Settings": "Tim Guan-tin Chien",
-  "Gaia::SMS": "David Scravaglieri",
-  "Gaia::System": "Gregor Wagner (2nd: Michael Henretty)", //  Tim relies on Gregor to actively throwing bugs over.
-  "Gaia::System::Browser Chrome": "Gregor Wagner (2nd Ben Francis)",
+  "Gaia::SMS": "David Scravaglieri (Anthony Ricaud)",
+  "Gaia::System": "Gregor Wagner (Michael Henretty)", //  Tim relies on Gregor to actively throwing bugs over.
+  "Gaia::System::Browser Chrome": "Gregor Wagner (Ben Francis)",
   "Gaia::System::Input Mgmt": "Tim Guan-tin Chien",
   "Gaia::System::Lockscreen": "Tim Guan-tin Chien",
   "Gaia::System::Window Mgmt": "Tim Guan-tin Chien",
   // "Gaia::TestAgent": "?",
   // "Gaia::UI Tests": "?",
-  "Gaia::Video": "Hema Koka",
+  "Gaia::Video": "Hema Koka (David Flanagan)",
   // "Gaia::Wallpaper": "?",
-  "Gaia::Wappush": "David Scravaglieri",
+  "Gaia::Wappush": "David Scravaglieri (Anthony Ricaud)",
   // "General": "?", // Firefox OS
   "GonkIntegration": "Thomas Tsai", // Firefox OS
   "General Automation": "Jonathan Griffin", // Release Engineering
@@ -64,7 +64,7 @@ var OWNERS = {
   // "Hardware": "?", // Firefox OS
   "Hardware Abstraction Layer (HAL)": "Dave Hylands", // Core
   "ImageLib": "Milan Sreckovic (Jeff Muizelaar, Seth Fowler)",
-  "IPC": "Andrew Overholt (2nd: Johnny Stenback)",
+  "IPC": "Andrew Overholt (Johnny Stenback)",
   "JavaScript Engine": "Naveed Ihsanullah", // Core
   "JavaScript: GC": "Naveed Ihsanullah", // Core
   "Layout": "Jet Villegas", // Core
@@ -73,7 +73,7 @@ var OWNERS = {
   "NFC": "Ken Chang", // Firefox OS
   "Panning and Zooming": "Milan Sreckovic (Kartikaya Gupta, Botond Ballo)", // Core
   "Performance": "Mike Lee", // Firefox OS
-  "Runtime": "Gregor Wagner (2nd: Fabrice Desré)", // Firefox OS
+  "Runtime": "Gregor Wagner (Fabrice Desré)", // Firefox OS
   // "Simulator": "?", // Firefox OS
   "RIL": "Ken Chang", // Firefox OS
   "Video/Audio": "Jet Villegas", // Core
